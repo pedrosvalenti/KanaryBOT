354ef95202684933b6749cd2aa8104920dd2df53
Pedro Seudo Valenti <pedrozzykkj@hotmail.com>
2025-11-08 11:57:21 -0300

    Corrigida cor do botão

--- src/commands/beijar.js
+++ src/commands/beijar.js
@@ -92,7 +92,7 @@ module.exports = {
         new ButtonBuilder()
           .setCustomId('retribuir')
           .setLabel('Retribuir 💞')
-          .setStyle(ButtonStyle.Danger)
+          .setStyle(ButtonStyle.Primary)
           .setDisabled(true)
       );
       try {
